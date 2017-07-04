d5401163a824894a6bf4193d3451b73d60295ff3
Thorsten Zoerner <me@thorsten-zoerner.com>
2017-07-04 14:38:31 +0200

    Updated BO Loader

---
+++ StromDAO-Browser/showcase/sc1_3.js
@@ -0,0 +1,9 @@
+$('#go').on('click',function() {
+	// Create Instance with external ID 1234 and existing private Key (relink)
+	var node = new document.StromDAOBO.Node({external_id:'1234',privateKey:$('#pk').val(),testMode:true,rpc:"https://demo.stromdao.de/rpc",abilocation:"https://cdn.rawgit.com/energychain/StromDAO-BusinessObject/6dc9e073/smart_contracts/"});
+	$('#result').show();
+	// Fill View (HTML) using JQuery
+	$('#account').html(node.wallet.address);
+	$('#private_key').val(node.wallet.privateKey);
+	$('#dsp_extid').val($('#extid').val());
+});
