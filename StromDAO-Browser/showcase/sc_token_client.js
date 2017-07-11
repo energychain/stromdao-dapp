b1c817087da0dd0bbd2c4fdffeecaa49c7d980b8
Thorsten Zoerner <me@thorsten-zoerner.com>
2017-07-11 14:12:08 +0200

    Updated BO Loader

---
+++ StromDAO-Browser/showcase/sc_token_client.js
@@ -0,0 +1,31 @@
+var tkn="0x8263026C63a95E312478203D54A56202D8B4a8eb";
+var node = new document.StromDAOBO.Node({external_id:'1234',testMode:true,rpc:"https://demo.stromdao.de/rpc",abilocation:"https://cdn.rawgit.com/energychain/StromDAO-BusinessObject/9147b186/smart_contracts"});
+
+$('#address').html(node.wallet.address);
+
+
+function updateBalance() {
+	node.erc20token(tkn).then(function(token) {		
+			token.balanceOf(node.wallet.address).then(function(b) {
+					$('#balance').html(parseInt(b,16));
+					if(parseInt(b,16)*1==0) {
+						$('.pay').attr('disabled','disabled');
+					} else {
+						$('.pay').removeAttr('disabled');
+					}
+			});
+	});
+}
+
+$('.pay').on('click',function(a,b) {
+	$(a.currentTarget).attr('disabled','disabled');
+	node.erc20token(tkn).then(function(token) {		
+			token.transfer(tkn,1).then(function(h) {
+					$('#history').append("<li><strong>"+h+"</strong><br/> "+$(a.currentTarget).html()+"</li>");
+					updateBalance();	
+					$(a.currentTarget).removeAttr('disabled');			
+			});
+	});	
+});
+setInterval("updateBalance();",10000);
+updateBalance();
---
+++ StromDAO-Browser/showcase/sc_token_info.js
@@ -0,0 +1,19 @@
+var tkn="0x8263026C63a95E312478203D54A56202D8B4a8eb";
+var node = new document.StromDAOBO.Node({external_id:'1234',testMode:true,rpc:"https://demo.stromdao.de/rpc",abilocation:"https://cdn.rawgit.com/energychain/StromDAO-BusinessObject/9147b186/smart_contracts"});
+
+function updateBalance() {
+	node.erc20token(tkn).then(function(token) {		
+			token.totalSupply().then(function(b) {
+					//console.log(b);
+					$('#sold').html(b);
+					$('#unused').html($('#sold').html()*1-$('#used').html());
+			});
+			token.balanceOf(tkn).then(function(b) {
+				$('#used').html(b);
+				$('#unused').html($('#sold').html()*1-$('#used').html());
+			});
+	});
+}
+
+setInterval("updateBalance();",10000);
+updateBalance();
